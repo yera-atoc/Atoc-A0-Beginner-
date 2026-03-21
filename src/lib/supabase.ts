bc6b6ffb1c4462ce343b85e2fe603b3d82a995ca
Yera-AtoC <ernar123123@gmail.com>
2026-03-21 10:29:32 +0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL || "https://xxxxx.supabase.co";
+  import.meta.env.VITE_SUPABASE_URL ?? "https://xxxxx.supabase.co";
 
 const supabaseAnonKey =
-  import.meta.env.VITE_SUPABASE_ANON_KEY || "ТВОЙ_ANON_KEY_СЮДА";
+  import.meta.env.VITE_SUPABASE_ANON_KEY ?? "eyJhbGciOi...";
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase ENV missing");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
